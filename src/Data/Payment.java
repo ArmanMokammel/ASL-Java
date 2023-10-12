8bdc278c107b2ec5395f65c3aa505dcf025af07b
SoraiyahSamantha <soraiyahsamantha@gmail.com>
2023-10-12 17:17:27 +0300

    added Payment.java class

---
+++ src/Data/Payment.java
@@ -0,0 +1,32 @@
+package Data;
+
+public class Payment {
+	
+	private String paymentType;
+	private double amount;
+	
+	public Payment (String paymentType, double amount) {
+		this.paymentType = paymentType;
+		this.amount = amount;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public String toString() {
+		return paymentType + "\t" + amount + "\n";
+	}
+}
